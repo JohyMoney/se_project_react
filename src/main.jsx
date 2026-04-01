cc639ece0f552510f5782aa3a4eaaec19145c5b9
JohnnyMoney <wyndrockjohn@gmail.com>
2026-04-01 13:13:12 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+=======
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+>>>>>>> 329f75f972eaf2bf79d1b134f31e9ab9b1992186
