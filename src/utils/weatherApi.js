624840d72be8d959339090ecd8a23d7329b33c55
JohnnyMoney <wyndrockjohn@gmail.com>
2026-04-09 15:49:34 -0400

    Add files via upload

---
+++ src/utils/weatherApi.js
@@ -0,0 +1,45 @@
+const getWeather = (coordinates, apiKey) => {
+  const { latitude, longitude } = coordinates;
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`,
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Error: ${res.status}`);
+  });
+
+  
+};
+
+const filterWeatherData = (data) => {
+  const result = {};
+
+  result.city = data.name;
+  result.temp = {
+    F: Math.round(data.main.temp),
+    C: Math.round(((data.main.temp - 32) * 5) / 9),
+  };
+  result.type = getWeatherType(data.main.temp);
+  result.isDay = isDay(data.dt, data.sys.sunrise, data.sys.sunset);
+  result.condition = data.weather[0].main;
+
+  return result;
+};
+
+const isDay = (currentTime, sunrise, sunset) => {
+  return sunrise < currentTime && currentTime < sunset;
+};
+
+const getWeatherType = (temperature) => {
+  if (temperature >= 86) {
+    return "hot";
+  } else if (temperature >= 66 && temperature < 86) {
+    return "warm";
+  } else if (temperature < 66) {
+    return "cold";
+  }
+};
+
+export { getWeather, filterWeatherData, getWeatherType, isDay };
