3f2cac55a18939d9fe7d5ff6cb654736ad618f73
JohnnyMoney <wyndrockjohn@gmail.com>
2026-05-24 11:20:27 -0400

    commit

--- src/components/EditProfileModal/EditProfileModal.jsx
+++ src/components/EditProfileModal/EditProfileModal.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext, useEffect } from "react";
 import useForm from "../../hooks/useForm.js";
 import CurrentUserContext from "../../contexts/CurrentUserContext.js";
@@ -67,73 +66,3 @@ function EditProfileModal({ isOpen, onClose, onUpdateProfile }) {
 }
 
 export default EditProfileModal;
-=======
-import { useContext, useEffect } from "react";
-import useForm from "../../hooks/useForm.js";
-import CurrentUserContext from "../../contexts/CurrentUserContext.js";
-import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
-
-const initialValues = {
-  name: "",
-  avatar: "",
-};
-
-function EditProfileModal({ isOpen, onClose, onUpdateProfile }) {
-  const currentUser = useContext(CurrentUserContext);
-  const { values, handleChange, setValues } = useForm(initialValues);
-
-  useEffect(() => {
-    if (isOpen) {
-      setValues({
-        name: currentUser?.name ?? "",
-        avatar: currentUser?.avatar ?? "",
-      });
-    }
-  }, [isOpen, currentUser, setValues]);
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    onUpdateProfile(values).catch(console.error);
-  };
-
-  return (
-    <ModalWithForm
-      onSubmit={handleSubmit}
-      buttonText="Save changes"
-      title="Change profile data"
-      isOpen={isOpen}
-      onClose={onClose}
-    >
-      <label htmlFor="edit-name" className="modal__label">
-        Name
-        <input
-          type="text"
-          className="modal__input"
-          id="edit-name"
-          name="name"
-          placeholder="Name"
-          required
-          minLength={2}
-          onChange={handleChange}
-          value={values.name}
-        />
-      </label>
-      <label htmlFor="edit-avatar" className="modal__label">
-        Avatar URL
-        <input
-          type="url"
-          className="modal__input"
-          id="edit-avatar"
-          name="avatar"
-          placeholder="Avatar URL"
-          required
-          onChange={handleChange}
-          value={values.avatar}
-        />
-      </label>
-    </ModalWithForm>
-  );
-}
-
-export default EditProfileModal;
->>>>>>> 583d20bafe86b377c2ad36fe6cfe4c459bc565e2
